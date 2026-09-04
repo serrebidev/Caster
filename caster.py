88c2da1e0916164ef7abf31953a7920182f6ac92
serrebidev <serrebi101@gmail.com>
2026-09-03 22:28:05 -0700

    Release v1.1.1

--- caster.py
+++ caster.py
@@ -59,7 +59,7 @@ from caster_extras import (
 )
 
 APP_TITLE = "Caster"
-APP_VERSION = "1.1.0"
+APP_VERSION = "1.1.1"
 
 YT_ID_RE = re.compile(
     r"(?:v=|youtu\.be/|/shorts/|/embed/)([A-Za-z0-9_-]{6,})"
